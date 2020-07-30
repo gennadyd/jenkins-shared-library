358693d64a72c11b84277b75e2ccca8b96f7e7ff
Davidov <gennady.davidov@Verint.com>
2020-07-30 18:00:35 +0300

    add get_results

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,3 +2,21 @@ def setDisplayName(displayText){
     println "setDisplayName"
     addShortText(text: displayText, borderColor: '#FF0000')
 }
+
+
+
+def get_results(param1) {
+
+    if (param1 == "fruit") {
+        return ["apple","orange","mango"]
+    } else {
+        return ["grape","peach","banana"]
+
+    }
+}
+
+// def readJsonParam(path){
+//     println "readJsonParam"
+//     readJSON file: "$path"
+//     return []
+// }
