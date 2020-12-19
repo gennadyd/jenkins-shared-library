45b0b64b4edee1baa7dea84c631adab7ac8d4cc1
Gennady Davidov <gdavidov@habana.ai>
2020-12-19 14:53:57 +0200

    add getKeepForeverBuilds

--- vars/jenkinsTools.groovy
+++ vars/jenkinsTools.groovy
@@ -15,4 +15,37 @@ class JenkinsTools {
         return jobs_list
     }
 
+   /**
+    * The function is deleting Keep Forever builds older than keepDays params
+    * @param String keepDays - days to keep "keep forever" builds
+    */
+    @NonCPS
+    def getKeepForeverBuilds(String keepDays="60"){
+        def result_success = []
+        def result_failed = []
+        Jenkins.instance.getAllItems().findAll { item -> (item instanceof Job) && !(((Job)item).builds.isEmpty()) } .each {
+            it.builds.findAll { build -> build.isKeepLog() } .each { keepForeverBuild ->
+                def absoluteURL = keepForeverBuild.getAbsoluteUrl()
+
+                use(groovy.time.TimeCategory) {
+                    def runningSince = groovy.time.TimeCategory.minus( new Date(), keepForeverBuild.getTime() )
+
+                    if(runningSince.getDays().toInteger() > keepDays.toInteger()) {
+                        try {
+                            keepForeverBuild.delete()
+                            println "Deleting ${absoluteURL} runningSince: ${runningSince} ago"
+                            result_success.add("${absoluteURL}")
+                        }
+                        catch (err) {
+                            println "Failed to delete ${absoluteURL} runningSince: ${runningSince} ago"
+                            result_failed.add("${absoluteURL}")
+                            throw err
+                        }
+                    }
+                }
+            }
+        }
+        return [result_success, result_failed]
+    }
+
 }
